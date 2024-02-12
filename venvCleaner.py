aeff29855d7e824bc72c9b619cb016019d9f5b5e
JustCati <ale.catinello@gmail.com>
2024-02-12 22:05:51 +0000

    feat: add support for home directory as argument

--- venvCleaner.py
+++ venvCleaner.py
@@ -8,14 +8,18 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="Clean up the virtual environment")
-    parser.add_argument("--path", default=os.path.join(os.path.expanduser('~'), ".local", "share", "virtualenvs", ""), help="Path to the virtual environments directory")
+    parser.add_argument("-u", "--user", help="User Path", required=True)
+    parser.add_argument("--path", default="", help="Path to the virtual environments directory")
     parser.add_argument("-t", "--time", default=3600, help="Time in seconds to wait before checking for new virtual environments (default: 3600)", type=int)
     parser.add_argument("--ignore-list", default=[], help="List of virtual environments NAMES to ignore", nargs="*")
     args = parser.parse_args()
 
-
-    if not os.path.exists(args.path):
-        raise ValueError("The path f{args.path} does not exist")
+    if args.path != "" and not os.path.exists(args.path):
+        raise ValueError(f"The path {args.path} does not exist")
+    
+    path = os.path.join(args.user, ".local", "share", "virtualenvs", "")
+    if not os.path.exists(path):
+        raise ValueError(f"The path {path} does not exist")
 
     while True:
         venvs = []
